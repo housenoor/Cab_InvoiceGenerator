de8bef0bffc8cbffcd1cbb5b19f5b4cb7664caed
Srikanth <srikanthh994@gmail.com>
2021-11-11 11:22:56 +0530

    Added:Test Case For Premium Rides

--- InvoiceServiceTest.java
+++ InvoiceServiceTest.java
@@ -5,16 +5,10 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class InvoiceServiceTest {
-	InvoiceGenerator invoiceGenerator = null;
-
-	@Before
-	public void setUp() throws Exception {
-		invoiceGenerator = new InvoiceGenerator();
-	}
 
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		invoiceGenerator = new InvoiceGenerator();
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -23,7 +17,7 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-		invoiceGenerator = new InvoiceGenerator();
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -31,23 +25,40 @@ public class InvoiceServiceTest {
 	}
 
 	@Test
-	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-		invoiceGenerator = new InvoiceGenerator();
-		Ride[] rides = { new Ride(25.0, 30), new Ride(12.0, 20) };
+	public void givenMultipleRides_shouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
-		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 420);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
 		Assertions.assertEquals(expectedInvoiceSummary, summary);
 	}
 
 	@Test
 	public void givenUserId_shouldReturnInvoiceSummary() {
-		invoiceGenerator = new InvoiceGenerator();
-		String userId = "srikanthh994@gmail.com";
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		String userId = "sunilgollapalli@gmail.com";
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 		invoiceGenerator.addRides(userId, rides);
-		InvoiceSummary summary = invoiceGenerator.calculateFare(rides, "normal");
-		InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(userId);
-		Assertions.assertEquals(invoiceSummary, summary);
+		InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+		Assertions.assertEquals(expectedInvoiceSummary, summary);
+	}
 
+	@Test
+	public void givenUserIdAndRides_ShouldReturn_MultipleInvoiceSummary() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		String userId = "sunilgollapalli@gmail.com";
+		Ride[] rides = { new Ride(2.0, 5, InvoiceGenerator.RideMode.NORMAL),
+				new Ride(0.1, 1, InvoiceGenerator.RideMode.NORMAL) };
+		invoiceGenerator.addRides(userId, rides);
+		Ride[] rides1 = { new Ride(2.0, 5, InvoiceGenerator.RideMode.PREMIUM),
+				new Ride(0.1, 1, InvoiceGenerator.RideMode.PREMIUM) };
+		invoiceGenerator.addRides(userId, rides1);
+		Ride[] rides2 = { new Ride(2.0, 5, InvoiceGenerator.RideMode.NORMAL),
+				new Ride(0.1, 1, InvoiceGenerator.RideMode.PREMIUM) };
+		invoiceGenerator.addRides(userId, rides2);
+		InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(6, 125);
+		Assertions.assertEquals(expectedInvoiceSummary, summary);
 	}
 }
--- Ride.java
+++ Ride.java
@@ -1,11 +1,17 @@
 package com.bridgelabz;
 
 public class Ride {
-    public final int time;
-    public double distance;
-    //parameterized Constructor
-    public Ride(double distance, int time){
-        this.distance = distance;
-        this.time = time;
-    }
+	public final int time;
+	public double distance;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+	public Ride(double distance, int time, InvoiceGenerator.RideMode rideMode) {
+		this.distance = distance;
+		this.time = time;
+	}
+
 }
